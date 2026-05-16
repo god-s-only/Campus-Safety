46222d4a94edf2e3ba8e643cf933f189ae9f6f21
god-s-only <joshxchris098@gmail.com>
2026-05-16 12:46:40 +0100

    feat: add IncidentCategory enum

---
+++ report/domain/src/main/java/com/caleb/campussafety/report/domain/model/IncidentCategory.kt
@@ -0,0 +1,12 @@
+package com.caleb.campussafety.report.domain.model
+
+enum class IncidentCategory {
+    THEFT,
+    HARASSMENT,
+    ASSAULT,
+    MEDICAL_EMERGENCY,
+    FIRE,
+    SUSPICIOUS_ACTIVITY,
+    VANDALISM,
+    OTHER
+}
