327212484363f11ab234ef8fec138c827f744049
god-s-only <joshxchris098@gmail.com>
2026-05-17 20:48:43 +0100

    refactor: remove manual use case providers from ReportModule

--- report/data/src/main/java/com/caleb/campussafety/report/data/di/ReportModule.kt
+++ report/data/src/main/java/com/caleb/campussafety/report/data/di/ReportModule.kt
@@ -2,10 +2,6 @@ package com.caleb.campussafety.report.data.di
 
 import com.caleb.campussafety.report.data.repository.ReportRepositoryImpl
 import com.caleb.campussafety.report.domain.repository.ReportRepository
-import com.caleb.campussafety.report.domain.usecase.GetCurrentLocationUseCase
-import com.caleb.campussafety.report.domain.usecase.GetIncidentsUseCase
-import com.caleb.campussafety.report.domain.usecase.SubmitReportUseCase
-import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,22 +17,4 @@ object ReportModule {
     fun provideReportRepository(
         impl: ReportRepositoryImpl
     ): ReportRepository = impl
-
-    @Provides
-    @Singleton
-    fun provideSubmitReportUseCase(
-        repository: ReportRepository
-    ): SubmitReportUseCase = SubmitReportUseCase(repository)
-
-    @Provides
-    @Singleton
-    fun provideGetIncidentsUseCase(
-        repository: ReportRepository
-    ): GetIncidentsUseCase = GetIncidentsUseCase(repository)
-
-    @Provides
-    @Singleton
-    fun provideGetCurrentLocationUseCase(
-        repository: ReportRepository
-    ): GetCurrentLocationUseCase = GetCurrentLocationUseCase(repository)
 }
