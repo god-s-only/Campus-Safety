c5d174b2250859a9184b90a0d0e3def4d7504688
god-s-only <joshxchris098@gmail.com>
2026-05-18 21:22:13 +0100

    feat: add OnLogoutClick to DashboardEvent

--- dashboard/presentation/src/main/java/com/caleb/campussafety/dashboard/presentation/dashboard/DashboardEvent.kt
+++ dashboard/presentation/src/main/java/com/caleb/campussafety/dashboard/presentation/dashboard/DashboardEvent.kt
@@ -5,6 +5,7 @@ import com.caleb.campussafety.report.domain.model.IncidentStatus
 sealed class DashboardEvent {
     data class OnFilterChange(val status: IncidentStatus?) : DashboardEvent()
     data class OnIncidentClick(val incidentId: String) : DashboardEvent()
+    object OnLogoutClick : DashboardEvent()
     data class OnUpdateStatus(
         val incidentId: String,
         val status: IncidentStatus
