cafdd4ec67d6364aac3cf8cfe4fb2c3b10a709f9
god-s-only <joshxchris098@gmail.com>
2026-05-18 21:18:12 +0100

    feat: add NavigateToLogin to StudentHomeAction

--- report/presentation/src/main/java/com/caleb/campussafety/report/presentation/home/StudentHomeAction.kt
+++ report/presentation/src/main/java/com/caleb/campussafety/report/presentation/home/StudentHomeAction.kt
@@ -3,5 +3,6 @@ package com.caleb.campussafety.report.presentation.home
 sealed class StudentHomeAction {
     object NavigateToReport : StudentHomeAction()
     object NavigateToHistory : StudentHomeAction()
+    object NavigateToLogin : StudentHomeAction()
     data class NavigateToIncidentDetail(val incidentId: String) : StudentHomeAction()
 }
