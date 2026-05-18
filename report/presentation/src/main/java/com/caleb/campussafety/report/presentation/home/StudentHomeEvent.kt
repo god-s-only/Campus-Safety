68c0317b190ce644cea6db836d4607955ee170e4
god-s-only <joshxchris098@gmail.com>
2026-05-18 21:17:51 +0100

    feat: add OnLogoutClick to StudentHomeEvent

--- report/presentation/src/main/java/com/caleb/campussafety/report/presentation/home/StudentHomeEvent.kt
+++ report/presentation/src/main/java/com/caleb/campussafety/report/presentation/home/StudentHomeEvent.kt
@@ -3,5 +3,6 @@ package com.caleb.campussafety.report.presentation.home
 sealed class StudentHomeEvent {
     object OnReportIncidentClick : StudentHomeEvent()
     object OnViewHistoryClick : StudentHomeEvent()
+    object OnLogoutClick : StudentHomeEvent()
     data class OnIncidentClick(val incidentId: String) : StudentHomeEvent()
 }
