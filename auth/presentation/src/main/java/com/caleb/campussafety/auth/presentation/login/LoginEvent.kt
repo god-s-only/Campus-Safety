361d476f624f809564228f76556c2b1e158dd1c7
god-s-only <joshxchris098@gmail.com>
2026-05-19 20:49:54 +0100

    feat: add OnRoleChange to LoginEvent

--- auth/presentation/src/main/java/com/caleb/campussafety/auth/presentation/login/LoginEvent.kt
+++ auth/presentation/src/main/java/com/caleb/campussafety/auth/presentation/login/LoginEvent.kt
@@ -1,8 +1,11 @@
 package com.caleb.campussafety.auth.presentation.login
 
+import com.caleb.campussafety.auth.domain.model.UserRole
+
 sealed class LoginEvent {
     data class OnEmailChange(val email: String) : LoginEvent()
     data class OnPasswordChange(val password: String) : LoginEvent()
+    data class OnRoleChange(val role: UserRole) : LoginEvent()
     object OnTogglePasswordVisibility : LoginEvent()
     object OnLoginClick : LoginEvent()
 }
